0fdc852dca5e44b38179c117b08b5aa6316d6eaa
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-10-14 16:21:18 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GlobalStyle from './styles/global';
+import SigIn from './pages/Signin';
+import SignIn from './pages/Signin';
+
+const App: React.FC = () => (
+  <>
+  <SignIn />
+  <GlobalStyle />
+  </>
+);
+export default App;
