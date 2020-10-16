7ab590bc03e5c3ee3b220f4ad109cfb66c0d564a
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-10-15 21:22:14 -0300

    Add unform

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
 import SignUp from './pages/SignUp';
-import SignIn from './pages/Signin';
+// import SignIn from './pages/Signin';
 
 const App: React.FC = () => (
   <>
