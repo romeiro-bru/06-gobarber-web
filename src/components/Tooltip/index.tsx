7c2abdb16c1480a0ef19c34cd21afd9fe5c98978
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-10-16 22:27:19 -0300

    ClassName not required

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -3,7 +3,7 @@ import { Container } from './styles';
 
 interface TooltipProps {
     title: string;
-    className: string;
+    className?: string;
 }
 
 const Tooltip: React.FC<TooltipProps> = ({ title, className, children }) => {
