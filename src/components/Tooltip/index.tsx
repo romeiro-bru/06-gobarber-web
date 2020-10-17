78e0de45b5264cdf0e8bb3e2d46e2831bf0409fe
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-10-17 00:32:46 -0300

    Tooltip title -  error text. Receive className

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -6,7 +6,7 @@ interface TooltipProps {
     className?: string;
 }
 
-const Tooltip: React.FC<TooltipProps> = ({ title, className, children }) => {
+const Tooltip: React.FC<TooltipProps> = ({ title, className = '', children }) => {
     return (
         <Container className={className} >
             {children}
