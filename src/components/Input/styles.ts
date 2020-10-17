c4c5a8cfce0364bf4ed46b3b31cc2250a0b3cdc9
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-10-16 21:39:21 -0300

    Define isErrored type, set border color

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -3,6 +3,7 @@ import styled, { css } from 'styled-components';
 interface ContainerProps {
     isFocused: boolean;
     isFilled: boolean;
+    isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -20,6 +21,10 @@ export const Container = styled.div<ContainerProps>`
     & + div {
         margin-top: 8px;
     }
+    
+    ${(props) => props.isErrored && css`
+        border-color: #c53030;        
+    `}
 
     ${(props) => props.isFocused && css`
         color: #ff9000;
