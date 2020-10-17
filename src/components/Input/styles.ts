92bd65ee438801cb9b2894e0e997ea3e76a53b22
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-10-16 22:23:58 -0300

    Style Tooltip within input styles

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components';
+import Tooltip from '../Tooltip';
 
 interface ContainerProps {
     isFocused: boolean;
@@ -51,7 +52,7 @@ export const Container = styled.div<ContainerProps>`
     }
 `;
 
-export const Error = styled.div`
+export const Error = styled(Tooltip)`
     height: 20px;
     margin-left: 16px;
     svg {
