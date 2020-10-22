94bcf625a07300806469e81406bc7e4ee97f6f98
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-10-22 15:24:27 -0300

    Login through context

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
-import SignUp from './pages/SignUp';
+// import SignUp from './pages/SignUp';
 import SignIn from './pages/Signin';
 
-import authContext from './context/AuthContext';
-import AuthContext from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-  <AuthContext.Provider value={{ name: 'Bruna' }} >
+  <AuthProvider >
     <SignIn />
-  </AuthContext.Provider>
+  </AuthProvider>
   
   <GlobalStyle />
   </>
