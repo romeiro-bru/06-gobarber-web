29b6f6e07597906486aea779df55dec03b57f635
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-10-22 00:19:28 -0300

    Add auth, useContext hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
 import SignUp from './pages/SignUp';
-// import SignIn from './pages/Signin';
+import SignIn from './pages/Signin';
+
+import authContext from './context/AuthContext';
+import AuthContext from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-  <SignUp />
+  <AuthContext.Provider value={{ name: 'Bruna' }} >
+    <SignIn />
+  </AuthContext.Provider>
+  
   <GlobalStyle />
   </>
 );
