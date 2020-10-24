71fb104085e1fbd1d42e53148c8c248f6f661ef2
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-10-23 22:54:23 -0300

    Import only appprovider from hooks

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import SignIn from './pages/Signin';
 
 import ToastContainer from './components/ToastContainer';
 
-import { AuthProvider } from './hooks/auth';
-import { ToastProvider } from './hooks/toast';
 import AppProvider from './hooks';
 
 const App: React.FC = () => (
@@ -14,7 +12,7 @@ const App: React.FC = () => (
     <AppProvider>
      <SignIn />
     </AppProvider> 
-       
+
     <ToastContainer />
 
     <GlobalStyle />
