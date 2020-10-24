20c330c4b5673845755792656b84e9d6bde7ced6
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-10-24 19:07:30 -0300

    Show toast msg

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,45 +1,33 @@
 import React from 'react';
 import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
 
+import { ToastMessage } from '../../hooks/toast';
 import { Container, Toast } from './styles';
 
-const ToastContainer: React.FC = () => {
+interface ToastContainerProps {
+    messages: ToastMessage[];
+}
+
+const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
     return ( 
-        <Container >
-            <Toast hasDescription={true}>
+        <Container>
+         {messages.map((message) => (
+            <Toast
+             key={message.id}
+             type={message.type}
+             hasDescription={!!message.description}
+            >
                 <FiAlertCircle size={20} />
                 <div>
-                    <strong>Houve um erro</strong>
-                    <p>Não foi possível fazer o login</p>
+                    <strong>{message.title}</strong>
+                    {message.description && <p>{message.description}</p>}
                 </div>
 
                 <button type="button">
                     <FiXCircle size={18} />
                 </button>
             </Toast>
-
-            <Toast type="success" hasDescription={false}>
-                <FiAlertCircle size={20} />
-                <div>
-                    <strong>Houve um erro</strong>
-                </div>
-
-                <button type="button">
-                    <FiXCircle size={18} />
-                </button>
-            </Toast>
-
-            <Toast type="error" hasDescription={true}>
-                <FiAlertCircle size={20} />
-                <div>
-                    <strong>Houve um erro</strong>
-                    <p>Não foi possível fazer o login</p>
-                </div>
-
-                <button type="button">
-                    <FiXCircle size={18} />
-                </button>
-            </Toast>
+        ))}
         </Container>
     );
 };
