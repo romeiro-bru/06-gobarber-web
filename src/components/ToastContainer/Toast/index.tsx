59664146834489450e42fceea623326801e50a03
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-10-24 21:23:44 -0300

    Toast icons

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -1,10 +1,16 @@
 import React, { useEffect } from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
+import { FiAlertCircle,FiCheckCircle, FiInfo, FiXCircle } from 'react-icons/fi';
 import { ToastMessage, useToast } from '../../../hooks/toast';
 import { Container } from './styles';
 
 interface ToastProps {
     message: ToastMessage;
+}
+
+const icons = {
+    info: <FiInfo size={24} />,
+    error: <FiAlertCircle size={24} />,
+    success: <FiCheckCircle size={24} />
 }
 
 const Toast: React.FC<ToastProps> = ({ message }) => {
@@ -13,14 +19,17 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
         const timer =  setTimeout(() => {
             removeToast(message.id)
         }, 3000)
+
+        return () => {
+            clearTimeout(timer);
+        }
     }, [removeToast, message.id])
 
     return (
         <Container 
-            type={message.type}
-            hasDescription={!!message.description}
-        >
-            <FiAlertCircle size={20} />
+            type={message.type} hasDescription={!!message.description}>
+                {icons[message.type ||'info' ]}
+        
             <div>
                 <strong>{message.title}</strong>
                 {message.description && <p>{message.description}</p>}
