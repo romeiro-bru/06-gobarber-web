aaa555931941f566f4d58aa098eeb104c90e56cd
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-10-26 18:03:47 -0300

    Import AnimationContainer

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
-import { Form } from '@unform/web'; 
-import { FormHandles } from '@unform/core'; 
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 
 import * as Yup from 'yup';
 import { Link } from 'react-router-dom';
@@ -10,7 +10,7 @@ import { useToast } from '../../hooks/toast';
 import { useAuth } from '../../hooks/auth';
 import getValidationsErrors from '../../utils/getValidationErrors';
 import logoImg from '../../assets/logo.svg';
-import { Container, Content, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from './styles';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -26,13 +26,13 @@ const SignIn: React.FC = () => {
     const { signIn } = useAuth();
     const { addToast } = useToast();
 
-    const handleSubmit = useCallback(async (data: SignInFormData ) => {
+    const handleSubmit = useCallback(async (data: SignInFormData) => {
         try {
             formRef.current?.setErrors({})
 
             const schema = Yup.object().shape({
                 email: Yup.string().required('E-mail obrigatório')
-                .email('Digite um e-mail válido'),
+                    .email('Digite um e-mail válido'),
                 password: Yup.string().required('Senha obrigatória')
             })
 
@@ -49,7 +49,7 @@ const SignIn: React.FC = () => {
             if (err instanceof Yup.ValidationError) {
                 const errors = getValidationsErrors(err)
                 formRef.current?.setErrors(errors)
-            }    
+            }
 
             addToast({
                 type: 'error',
@@ -59,26 +59,28 @@ const SignIn: React.FC = () => {
         }
     }, [signIn, addToast]);
 
-    return (        
+    return (
         <Container>
             <Content>
-                <img src={logoImg} alt="GoBarber" />
+                <AnimationContainer>
+                    <img src={logoImg} alt="GoBarber" />
 
-                <Form ref={formRef} onSubmit={handleSubmit}>
-                    <h1>Faça seu logon</h1>
-                    <Input name="email" icon={FiMail} placeholder="E-mail" />
-                    <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
-                    <Button type="submit">Entrar</Button>
+                    <Form ref={formRef} onSubmit={handleSubmit}>
+                        <h1>Faça seu logon</h1>
+                        <Input name="email" icon={FiMail} placeholder="E-mail" />
+                        <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+                        <Button type="submit">Entrar</Button>
 
-                    <a href="forgot">Esqueci minha senha</a>              
-                </ Form>
-                <Link to="/signup">
-                    <FiLogIn />
-                    Criar conta
-                </Link>
+                        <a href="forgot">Esqueci minha senha</a>
+                    </ Form>
+                    <Link to="/signup">
+                        <FiLogIn />
+                         Criar conta
+                    </Link>
+                </AnimationContainer>
             </Content>
             <Background />
         </Container>
     );
 }
- export default SignIn;
+export default SignIn;
