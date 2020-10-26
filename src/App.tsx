992166edd30bcbd0a1815a26dd6ae96c70f69eb4
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-10-26 17:48:01 -0300

    Add BrowserRouter, Import Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
-// import SignUp from './pages/SignUp';
-import SignIn from './pages/Signin';
-
 import AppProvider from './hooks';
+import Routes from './routes';
 
 const App: React.FC = () => (
-  <>
+  <BrowserRouter>
     <AppProvider>
-     <SignIn />
+     <Routes />
     </AppProvider> 
 
     <GlobalStyle />
-  </>
+  </ BrowserRouter>
 );
+
 export default App;
