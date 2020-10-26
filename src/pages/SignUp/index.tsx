2c2d46e447aec3632792b44f8fe1e4d516d79685
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-10-26 18:24:01 -0300

    Import AnimationContainer

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 import getValidationsErrors from '../../utils/getValidationErrors' ;
 
 import logoImg from '../../assets/logo.svg';
-import { Container, Content, Background } from './styles';
+import { Container, Content, Background, AnimationContainer } from './styles';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -42,21 +42,23 @@ const SignUp: React.FC = () => {
         <Container>
         <Background />
         <Content>
-            <img src={logoImg} alt="GoBarber" />
+            <AnimationContainer>
+                <img src={logoImg} alt="GoBarber" />
 
-            <Form ref={formRef} onSubmit={handleSubmit}>
-                <h1>Faça seu cadastro</h1>
-                <Input name="name" icon={FiUser} placeholder="Nome" />
-                <Input name="email" icon={FiMail} placeholder="E-mail" />
+                <Form ref={formRef} onSubmit={handleSubmit}>
+                    <h1>Faça seu cadastro</h1>
+                    <Input name="name" icon={FiUser} placeholder="Nome" />
+                    <Input name="email" icon={FiMail} placeholder="E-mail" />
 
 
-                <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
-                <Button type="submit">Cadastrar</Button>
-            </Form>
-            <Link to="/">
-                <FiArrowLeft />
-                Voltar para logon
-            </Link>
+                    <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+                    <Button type="submit">Cadastrar</Button>
+                </Form>
+                <Link to="/">
+                    <FiArrowLeft />
+                    Voltar para logon
+                </Link>
+            </AnimationContainer>
         </Content>
     </Container>
     )
