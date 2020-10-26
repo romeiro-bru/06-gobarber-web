b1d251c5da672018506e90995320848fd7af2984
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-10-26 20:46:09 -0300

    Add dashboard private route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,15 @@ import { Switch, Route } from 'react-router-dom';
 
 import SignIn from '../pages/Signin';
 import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
     <Switch>
         <Route path="/" exact component={SignIn} />
         <Route path="/signup" component={SignUp} />
+
+        <Route path="/dashboard" component={Dashboard} isPrivate />
+
 
     </Switch>
 )
