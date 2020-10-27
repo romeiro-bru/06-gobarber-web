2798817f49fd5d80a72ece3898b4d357d6584b64
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-10-26 21:22:22 -0300

    Import Route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/Signin';
 import SignUp from '../pages/SignUp';
