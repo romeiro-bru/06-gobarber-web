10c6ad3e033aa9d05bea3fc76bb325c8f9a0600d
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-10-26 21:30:43 -0300

    Add location to keep track of pages navigation

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -16,10 +16,12 @@ const Route: React.FC<RouteProps> = ({ isPrivate = false, component: Component, 
     return (
     <ReactDOMRoute
         {...rest}
-        render={() => {
+        render={({ location }) => {
             return isPrivate == !!user ? (
                 <Component /> 
-            ) : ( <Redirect to={{ pathname: isPrivate ? '/' : '/dashboard' }} /> )
+            ) : ( <Redirect to={{ pathname: isPrivate ? '/' : '/dashboard',
+            state: { from: location } 
+            }} /> )
         }}
     />)
 }
